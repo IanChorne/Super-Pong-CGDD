9bf7ff175ab984db3f8ffabd3e072782805c3327
IChorne <114964724+IChorne@users.noreply.github.com>
2023-03-14 14:34:20 -0400

    Update MainMenu.cs

--- Super-Pong/Assets/Scripts/MainMenu.cs
+++ Super-Pong/Assets/Scripts/MainMenu.cs
@@ -5,6 +5,7 @@ using System.IO;
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class MainMenu : MonoBehaviour
 {
@@ -42,5 +43,8 @@ public class MainMenu : MonoBehaviour
     public void quitGame()
     {
         Application.Quit();
+
+        //Exit the editor as well
+        EditorApplication.isPlaying = false;
     }
 }
